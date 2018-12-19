8c9e1a048d08cda995ce29239b8d7da4082c16df
Ward <vakpko@gmail.com>
2018-12-19 12:40:22 +0100

    xc

--- src/App.js
+++ src/App.js
@@ -4,21 +4,36 @@ import './App.css';
 
 class App extends Component {
   render() {
+    let store=[];
+    store.push({
+    name: 'Hendo',
+    profs:{ city:'Glasgow', smoke: 'chesterfield'}
+  },
+  {
+    name: 'Benjamin',
+    profs:{ city:'Cardiff', smoke: 'Sigaronne'}
+  });
+
+    console.log(store.map(i=> i.profs.city))
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+         
+          <div>
+
+          <form onSubmit={this.handleSubmit}>
+          <input type='text'  placeholder='search City' />
+  
+        <label>
+          EssayJSON:
+          <textarea  onChange={this.handleChange} />
+        </label>
+        <button type='submit'> Submit</button>
+          </form>
+          </div>
+
+       
         </header>
       </div>
     );
